876f44e703b8d20712bdd850d120e3fc9cd1da9b
Jzrt <32945597+Jzrt@users.noreply.github.com>
2020-10-15 20:48:20 +0800

    Update upload.sh

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 rm -rf KindleEar
 git clone https://github.com/Jzrt/KindleEar.git
 python helper.py
-appcfg.py update KindleEar/app.yaml KindleEar/module-worker.yaml
-appcfg.py update KindleEar/
+appcfg.py update --noauth_local_webserver KindleEar/app.yaml KindleEar/module-worker.yaml
+appcfg.py update --noauth_local_webserver KindleEar/
